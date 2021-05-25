1df7e8a945c15ad90f978374621f14f730a5503c
arpitpannu <38281985+arpitpannu@users.noreply.github.com>
2021-05-25 20:33:33 +0530

    Add files via upload

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./index.css";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div className="logo">
+                    <h1>Stock Market</h1>
+                </div>
+            </div>
+        );
+    }
+}
+export default Home;
